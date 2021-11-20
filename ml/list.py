4e63e79db62ae8df117447bcf9402848313286a5
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-11-20 19:07:29 +0530

    Create list.py

---
+++ ml/list.py
@@ -0,0 +1,22 @@
+#1d list in python
+d1=[i for i in range(5)]
+print(d1)
+
+#1d list in python
+m=5
+d1=[0]*m
+print(d1)
+
+
+#2d list in python
+d2=[[j for i in range(5)] for j in range(5)]
+print(d2)
+
+#2d list in python
+r,c=3,5
+d1=[[0]*c]*r
+print(d1)
+
+
+
+
